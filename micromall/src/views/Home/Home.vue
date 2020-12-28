2019ed2c3f3340fa9c0a9f096bf93fc1a824bb14
KingRY123000 <KingRY@users.noreply.github.com>
2020-12-28 09:33:07 +0800

    Initial commit

--- micromall/src/views/Home/Home.vue
+++ micromall/src/views/Home/Home.vue
@@ -1,13 +1,27 @@
 <template>
-
+  <div>
+    <navigation-bar class="home-nav">
+      <div slot="center">
+        购物街
+      </div>
+    </navigation-bar>
+  </div>
 </template>
 
 <script>
+import NavigationBar from "@/components/commons/Navigation/NavigationBar";
+
 export default {
-  name: "Home"
+  name: "Home",
+  components: {
+    NavigationBar,
+  }
 }
 </script>
 
 <style scoped>
-
+.home-nav {
+  background: var(--color-tint);
+  color: #fff;
+}
 </style>
