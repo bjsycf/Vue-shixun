005d0aeb7bd8fd0ca423a639ac2d5e055fe68ac0
KingRY123000 <KingRY@users.noreply.github.com>
2020-12-29 08:22:39 +0800

    12.28

--- micromall/src/components/contexts/GoodsList/GoodsListItem.vue
+++ micromall/src/components/contexts/GoodsList/GoodsListItem.vue
@@ -2,9 +2,9 @@
   <div class="goods-item">
     <img v-bind:src="goodsItem.show.img">
     <div class="goods-info">
-      <p>{{goodsItem.title}}</p>
-      <span class="price">{{goodsItem.price}}</span>
-      <span class="collect">{{goodsItem.cfav}}</span>
+      <p>{{ goodsItem.title }}</p>
+      <span class="price">{{ goodsItem.price }}</span>
+      <span class="collect">{{ goodsItem.cfav }}</span>
     </div>
   </div>
 </template>
@@ -13,10 +13,10 @@
 
 export default {
   name: "GoodsListItem",
-  props:{
+  props: {
     goodsItem: {
       type: Object,
-      default(){
+      default() {
         return {}
       }
     }
@@ -30,10 +30,12 @@ export default {
   position: relative;
   width: 48%;
 }
+
 .goods-item img {
   width: 100%;
   border-radius: 5px;
 }
+
 .goods-info {
   font-size: 12px;
   position: absolute;
@@ -51,6 +53,7 @@ export default {
   margin-bottom: 3px;
 
 }
+
 .goods-info .price {
   color: var(--color-high-text);
   margin-right: 20px;
@@ -59,4 +62,24 @@ export default {
 .goods-info .collect {
   position: relative;
 }
+
+.goods-info .collect::before {
+  content: '';
+  position: absolute;
+  left: -15px;
+  top: -1px;
+  width: 14px;
+  height: 14px;
+}
+
+.goods-info .collect::before {
+  content: '';
+  position: absolute;
+  left: -15px;
+  top: -1px;
+  width: 14px;
+  height: 14px;
+  background: url("../../../assets/img/common/collect.svg") 0 0/14px 14px;
+}
+
 </style>
