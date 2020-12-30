bf78c582c4075762259c68e8dc9a971db0be4445
KingRY123000 <KingRY@users.noreply.github.com>
2020-12-30 16:38:44 +0800

    12.30

--- micromall/src/views/Detail/Detail.vue
+++ micromall/src/views/Detail/Detail.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="detail">
     <detail-navigation-bar class="detail-nav"></detail-navigation-bar>
-
+    <!--使用前面封装的Scroll.vue，进行滚动效果的调整-->
     <scroll class="content" ref="scroll">
       <detail-swiper :top-images="topImages"></detail-swiper>
       <detail-base-info :goods="goods"/>
@@ -56,40 +56,32 @@ export default {
     GoodsList,
   },
   created() {
-    // 1.保存传入的iid
+    //保存传入的iid
     this.iid = this.$route.params.iid
-
-    // 2.根据iid请求详情数据
+    //根据iid请求详情数据
     getDetail(this.iid).then(res => {
-      // 1.获取顶部的图片轮播数据
+      //获取顶部的图片轮播数据
       console.log(res);
       const data = res.result;
       this.topImages = data.itemInfo.topImages
-
-      // 2.获取商品信息
+      //获取商品信息
       this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
-
-      // 3.创建店铺信息的对象
+      //创建店铺信息的对象
       this.shop = new Shop(data.shopInfo)
-
-      // 4.保存商品的详情数据
+      //保存商品的详情数据
       this.detailInfo = data.detailInfo;
-
-
-      // 5.获取参数的信息
+      //获取参数的信息
       this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
-
-      // 6. 获取评论信息
+      //获取评论信息
       if (data.rate.cRate !== 0) {
         this.commentInfo = data.rate.list[0]
       }
     })
-
+    //展示推荐商品信息
     getRecommends().then(res => {
       console.log(res)
       this.recommends = res.data.list
     })
-
   },
 
   methods: {
@@ -97,7 +89,6 @@ export default {
       this.$refs.scroll.refresh()
     }
   },
-
 }
 </script>
 
